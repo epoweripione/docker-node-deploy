1552b6c23a78abefb57acc21250e638b15cb0652
siyuwuxin <siyuwuxin@gmail.com>
2019-04-27 21:42:10 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,13 +31,8 @@ if [ ! -f "/opt/node/webhook/index.js" ]; then
 		# npm install github-webhook-handler
 		sed -i "s/WEBHOOK-HANDLER/github-webhook-handler/" /opt/node/webhook/index.js
 
-		rm -rf /opt/node/app && mkdir -p /opt/node
-		cd /opt/node && git clone $GITHUB ./app
-
-		cd /opt/node/webhook
-		pm2 start index.js --name webhook
-
-		/opt/node/webhook/deploy.sh
+		rm -rf /opt/node/app && mkdir -p /opt/node/
+		cd /opt/node/ && git clone $GITHUB /opt/node/app
 	fi
 
 	# Gitlab webhook
@@ -45,13 +40,8 @@ if [ ! -f "/opt/node/webhook/index.js" ]; then
 		# npm install node-gitlab-webhook
 		sed -i "s/WEBHOOK-HANDLER/node-gitlab-webhook/" /opt/node/webhook/index.js
 
-		rm -rf /opt/node/app && mkdir -p /opt/node
-		cd /opt/node && git clone $GITLAB ./app
-
-		cd /opt/node/webhook
-		pm2 start index.js --name webhook
-
-		/opt/node/webhook/deploy.sh
+		rm -rf /opt/node/app && mkdir -p /opt/node/
+		cd /opt/node/ && git clone $GITLAB ./app
 	fi
 
 	# Gogs webhook
@@ -59,15 +49,28 @@ if [ ! -f "/opt/node/webhook/index.js" ]; then
 		# npm install gogs-webhook-handler
 		sed -i "s/WEBHOOK-HANDLER/gogs-webhook-handler/" /opt/node/webhook/gogs.js
 
-		rm -rf /opt/node/app && mkdir -p /opt/node
-		cd /opt/node && git clone $GOGS ./app
-
-		cd /opt/node/webhook
-		pm2 start gogs.js --name webhook
-
-		/opt/node/webhook/deploy.sh
+		rm -rf /opt/node/app && mkdir -p /opt/node/
+		cd /opt/node/ && git clone $GOGS ./app
 	fi
-else
-	cd /opt/node/webhook
-	pm2 start index.js --name webhook
 fi
+
+
+# Start webhook
+cd /opt/node/webhook
+pm2 start index.js --name webhook
+
+# Start app
+# [ -z "$PM2_CONFIG_FILE" ] && PM2_CONFIG_FILE=pm2.json
+# pm2 start ${PM2_CONFIG_FILE}
+/opt/node/webhook/deploy.sh
+
+
+# Tail log file to keep container running
+[ -z "$TAILLOG" ] && export TAILLOG=/var/log/*.log
+tail -f $TAILLOG
+
+
+# # crond -b -L /var/log/crond.log
+# # nginx -g "daemon off;"
+
+# exec "$@"
