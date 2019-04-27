30d5ca31d31b05c21c648be96baa4f46e6af85f3
siyuwuxin <siyuwuxin@gmail.com>
2019-04-27 22:00:01 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ pm2 describe ${APP_NAME} > /dev/null
 RUNNING=$?
 
 if [ "$RUNNING" -ne 0 ]; then
-    pm2 start ${PM2_CONFIG_FILE}
+    pm2 start ${PM2_CONFIG_FILE} --no-daemon
 else
     pm2 sendSignal SIGUSR2 ${PM2_CONFIG_FILE}
 fi
