f59e9cc7e72fdacb356c33342770de9dad87d0ca
siyuwuxin <siyuwuxin@gmail.com>
2019-04-27 21:40:38 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -10,15 +10,15 @@ echo "Pulling source code..."
 git pull origin master
 
 echo "Generate and deploy..."
-[ -z $PM2_CONFIG_FILE ] && PM2_CONFIG_FILE=pm2.json
+[ -z "$PM2_CONFIG_FILE" ] && PM2_CONFIG_FILE=pm2.json
 # pm2 start pm2.json
 # pm2 sendSignal SIGUSR2 pm2.json
 
-[ -z $APP_NAME ] && APP_NAME=nodeapp
+[ -z "$APP_NAME" ] && APP_NAME=nodeapp
 pm2 describe ${APP_NAME} > /dev/null
 RUNNING=$?
 
-if [ "${RUNNING}" -ne 0 ]; then
+if [ "$RUNNING" -ne 0 ]; then
     pm2 start ${PM2_CONFIG_FILE}
 else
     pm2 sendSignal SIGUSR2 ${PM2_CONFIG_FILE}
